768a0c734520801437a1a8613e4e0ef12d0d0367
Kathleen Calmerin <kthlndznclmrn@gmail.com>
2017-05-15 09:32:13 +0800

    Uploaded the acceptedcontent.php and accepted.php

---
+++ accepted.php
@@ -0,0 +1,4 @@
+<?php include ("headernavbar.php"); ?>
+<?php include ("mainsidebar.php"); ?>
+<?php include ("acceptedcontent.php"); ?>
+<?php include ("mainfooter.php"); ?>
---
+++ acceptedcontent.php
@@ -0,0 +1,60 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+
+    <link rel="stylesheet" type="text/css" href="DataTables/datatables.min.css"/>
+    <h1>
+      YOUR Accepted Requests
+      <span class="glyphicon glyphicon-list-alt"></span>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+      <li class="active">Here</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+   <?php 
+   require('db/acceptedRequests.php');
+   // TODO: // put id
+   $result = acceptedRequests(101); 
+
+   // while($row = $result->fetch_assoc()) {
+   //   echo $row['lname'];
+   // }
+   ?>
+   <h3> Invoices </h3>           
+   <table class="table table-hover data-table">
+    <thead>
+      <tr>
+       
+       <th>Last Name</th>
+       <th>First Name</th>
+       <th>Service Type</th>
+       <th>Timestamp</th>
+
+     </tr>
+   </thead>
+   <tbody>
+
+    <?php while ($row = $result->fetch_assoc()): ?>
+      <tr>
+        <td><?php echo $row['fname'] ?></td>
+        <td><?php echo $row['lname'] ?></td>
+        <td><?php echo $row['service_name'] ?></td>
+        <td><?php echo $row['date'] ?></td>
+    
+        
+      </tr>
+    <?php endwhile ?>
+
+  </tbody>
+</table>
+
+
+</section>
+<!-- /.content -->
+</div>
+  <!-- /.content-wrapper -->
